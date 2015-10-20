e07169c18108a50042d8922fb0c34b1ed04f2031
Vikas K <vcoski@gmail.com>
2015-10-21 04:18:40 +0530

    Executing commands through c apis.

--- src/holodeck/pipe.c
+++ src/holodeck/pipe.c
@@ -2,28 +2,20 @@
 #include "tlpi_hdr.h"
 
 int main(int argc, char const *argv[]) {
-  pid_t pid;
   if(argc < 2) {
     printf("usage format %s command ... \n", argv[0]);
   }
   else {
-    if ((pid = fork()) == -1) {
-       perror("fork() error");
+    int limit = 1;
+    char *params[argc - limit + 1];
+    int index;
+    for(index=0; index<(argc - limit); index++) {
+      params[index] = (char *)malloc(strlen(argv[index+limit])+1);
+      strcpy(params[index], argv[index+limit]);
+      printf("%s\n", params[index]);
     }
-    else {
-      int limit = 1;
-      if(argc > limit) {
-        char *params[argc - limit + 1];
-        int index;
-        for(index=0; index<(argc - limit); index++) {
-          params[index] = (char *)malloc(strlen(argv[index+limit])+1);
-          strcpy(params[index], argv[index+limit]);
-          printf("%s\n", params[index]);
-        }
-        params[index] = NULL;
-        execvp(params[0], params);
-      }
-    }
+    params[index] = NULL;
+    execvp(params[0], params);
   }
 
   return 0;
