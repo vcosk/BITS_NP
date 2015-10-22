565cd3b4aab0230d89f4ecc7ecfb043332e6af4a
Vikas K <vcoski@gmail.com>
2015-10-22 16:23:43 +0530

    Made exec code common.

--- src/holodeck/pipe2.c
+++ src/holodeck/pipe2.c
@@ -2,62 +2,37 @@
 #include "tlpi_hdr.h"
 
 int main(int argc, char const *argv[]) {
-  // if(argc < 2) {
-  //   printf("usage format %s command ... \n", argv[0]);
-  // }
-  // else {
-  //   int limit = 1;
-  //   char *params[argc - limit + 1];
-  //   int index;
-  //   for(index=0; index<(argc - limit); index++) {
-  //     params[index] = (char *)malloc(strlen(argv[index+limit])+1);
-  //     strcpy(params[index], argv[index+limit]);
-  //     printf("%s\n", params[index]);
-  //   }
-  //   params[index] = NULL;
-  //   execvp(params[0], params);
-  // }
-  //
-  // return 0;
+  int command_pipe[2];
+  if(pipe(command_pipe) == -1) {
+    perror("Pipe failed");
+    exit(1);
+  }
 
-  int des_p[2];
-        if(pipe(des_p) == -1) {
-          perror("Pipe failed");
-          exit(1);
-        }
+  char* prog1[] = { "ls", "-l", 0};
+  char* prog2[] = { "wc", "-l", 0};
+  executeCommand(command_pipe, prog1, TRUE);
+  executeCommand(command_pipe, prog2, FALSE);
+  close(command_pipe[0]);
+  close(command_pipe[1]);
+  wait(0);
+  wait(0);
+  return 0;
+}
 
-        if(fork() == 0)        //first fork
-        {
-            close(0);          //closing stdin
-            dup(des_p[0]);     //replacing stdin with pipe read
-            close(1);          //closing stdout
-            dup(des_p[1]);     //replacing stdout with pipe write
-            close(des_p[0]);   //closing pipe read
-            close(des_p[1]);
+void executeCommand(int command_pipe[], char* prog[], int closeStdOut) {
+  if(fork() == 0)        //first fork
+  {
+    close(0);          //closing stdin
+    dup(command_pipe[0]);     //replacing stdin with pipe read
+    if(closeStdOut) {
+      close(1);          //closing stdout
+      dup(command_pipe[1]);     //replacing stdout with pipe write
+    }
+    close(command_pipe[0]);   //closing pipe read
+    close(command_pipe[1]);
 
-            const char* prog1[] = { "ls", "-l", 0};
-            execvp(prog1[0], prog1);
-            perror("execvp of ls failed");
-            exit(1);
-        }
-
-        if(fork() == 0)        //creating 2nd child
-        {
-            close(0);          //closing stdin
-            dup(des_p[0]);     //replacing stdin with pipe read
-            close(des_p[1]);   //closing pipe write
-            close(des_p[0]);
-
-            const char* prog2[] = { "wc", "-l", 0};
-            execvp(prog2[0], prog2);
-            perror("execvp of wc failed");
-            exit(1);
-        }
-
-        close(des_p[0]);
-        close(des_p[1]);
-        wait(0);
-        wait(0);
-
-        return 0;
+    execvp(prog[0], prog);
+    perror("execvp of ls failed");
+    exit(1);
+  }
 }
